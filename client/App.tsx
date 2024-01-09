d6e7e67af6a0179354bebc76531241965b786e7f
huwfernie <huwfernie@gmail.com>
2024-01-09 16:37:53 +0000

    first pass

--- client/App.tsx
+++ client/App.tsx
@@ -1,29 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Feed from './components/Feed';
 
 function App() {
+  const [feedData, setFeedData] = useState([]);
+  const [page, setPage] = useState(0);
+
+  function fetchNextPage() {
+    const _page = page + 1;
+    setPage(_page);
+  }
+
   useEffect(() => {
     async function init() {
-      const page = 0;
-      const briefref = "brief-DD650C75-1401-11ED-B757-0A9E4A196D19";
+      // const briefref = "brief-DD650C75-1401-11ED-B757-0A9E4A196D19";
 
       // FEED
-      let dataFeed = await fetch(`http://localhost:4000/feed?PAGE=${page}`);
-      dataFeed = await dataFeed.json();
-      console.log(dataFeed);
+      let newFeedData = await fetch(`http://localhost:4000/feed?PAGE=${page}`);
+      newFeedData = await newFeedData.json();
+      //@ts-ignore
+      setFeedData([...feedData, ...newFeedData]);
+      
 
       // COMMENTS
-      let dataComments = await fetch(`http://localhost:4000/comments/${briefref}`);
-      dataComments = await dataComments.json();
-      console.log(dataComments);
+      // let dataComments = await fetch(`http://localhost:4000/comments/${briefref}`);
+      // dataComments = await dataComments.json();
+      // console.log(dataComments);
       
     }
     init();
-  }, []);
+  }, [page]);
 
   return (
     <div className="App">
-      Hello, World
+      {/* @ts-ignore */}
+      <Feed data={feedData} fetchNextPage={fetchNextPage} />
     </div>
   );
 }
---
+++ client/components/Feed.js
@@ -0,0 +1,42 @@
+
+import React, { useEffect, useRef } from 'react';
+import './feed.css';
+import FeedItem from "./FeedItem";
+
+function Feed({ data, fetchNextPage }) {
+    const target = useRef();
+
+    try {
+        useEffect(() => {
+            function init() {
+                if (target.current !== undefined) {
+                    let observer = new IntersectionObserver(fetchNextPage);
+                    observer.observe(target.current);
+                } else {
+                    setTimeout(() => {
+                        init();
+                    }, 100);
+                }
+            }
+            init();
+        }, [])
+
+        if (data.length === 0) {
+            return null;
+        }
+
+        return (
+            <div className="feed">
+                {data.map((item, index) => {
+                    return <FeedItem key={index} data={item} />
+                })}
+                <span ref={target} className="fetch-next-page"></span>
+            </div>
+        );
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export default Feed;
